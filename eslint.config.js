d3ae09d1676bfc91c339d71883312262ae749235
Splines <37160523+Splines@users.noreply.github.com>
2024-01-04 19:16:39 +0100

    Update own config to new flat ESLint (#11)
    * Upgrade dependencies
    
    * Upgrade to ESLint v9 (alpha)
    
    * Upgrade own ESLint config to new flat config
    
    Also added package "globals"
    
    * Update folders that ESLint should ignore
    
    * Fix ESLint code violations
    
    * Add global ignore pattern to docs
    
    * Only test with newer versions of node
    
    * Only test on latest node version
    
    Otherwise, v9 of ESLint can't be installed
    
    * Run npm with `--legacy-peer-deps` flag
    
    This is because we are using an alpha version of ESLint.

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const js = require("@eslint/js");
+const stylistic = require("@stylistic/eslint-plugin");
+const globals = require("globals");
+
+const customizedStylistic = stylistic.configs.customize({
+  "indent": 2,
+  "jsx": false,
+  "quote-props": "always",
+  "semi": "always",
+  "brace-style": "1tbs",
+});
+
+module.exports = [
+  js.configs.recommended,
+  {
+    ignores: [
+      "node_modules/",
+      "tests/fixtures/",
+      "tmp/",
+    ],
+  },
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      ...customizedStylistic.rules,
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+    },
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+  },
+];
